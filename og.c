a56afcaa963f93d9c048ef7b6c92b778f2d6ce7a
Arnav <arnavmishra2911@gmail.com>
2026-07-04 15:39:33 +0530

    Electricity.c

--- og.c
+++ og.c
@@ -1,30 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // gcc is gnu c compiler which convert human language to computer language to execute code its fullform is GNU Compiler Collection
 int main(){ for (int i = 1; i <= 20; i++)
-{ 
-    if (i < 5)
-        continue;
+    {
+        if (i < 5)
+            continue;
 
-    int result;
+        int result;
 
-    if (i % 2)
-        result = i + 2;
-    else
-        result = i * 2;
+        if (i % 2)
+            result = i + 2;
+        else
+            result = i * 2;
 
-    if (result == 10)
-        continue;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        if (result == 10)
+            continue;
         
 
-    if (result >= 15)
-    {
-        printf("Program terminated.\n");
-        break;
-    }
+        if (result >= 15)
+        {
+            printf("Program terminated.\n");
+            break;
+        }
 
-    printf("%d\n", result);
-}
-return 0;
+        printf("%d\n", result);
+    }
+    return 0;
 }
 //constant values are those values which are same while running code
